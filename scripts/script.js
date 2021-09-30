83398c25974e87726616530e165b3b4c4086d642
elston-code <giovanni1942@live.nl>
2021-09-30 19:38:38 +0200

    menu added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+/******************************/
+/* Menu */
+/******************************/
+
+
+var menuButton = document.querySelector("header nav:nth-of-type(1) button")
+
+var menu = document.querySelector("header nav:nth-of-type(2)")
+ 
+
+function toggleMenu(){
+
+    menu.classList.toggle("menu-open")
+} 
+
+menuButton.addEventListener("click", toggleMenu)
