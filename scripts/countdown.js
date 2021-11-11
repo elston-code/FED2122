9194d2678966661d0cc1ca8552084610605eabf8
elston-code <giovanni1942@live.nl>
2021-11-11 16:37:10 +0100

    Update countdown.js

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,8 @@
 var timerSpan = document.querySelector('section > p span');
 var counter = 10;
 
+
+//functie voor aftellen 
 function updateCountdown(){
     timerSpan.innerHTML = counter + ' seconden';
     counter--;
@@ -9,7 +11,11 @@ function updateCountdown(){
     }
 }
 
+//terug naar homepagina
 function gaNaarHome(){
     window.location = 'index.html';
 }
+
+//tijdsduur voor aftellen
 setInterval(updateCountdown, 1000);
+
